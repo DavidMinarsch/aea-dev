64ba72ae4ffcb22f0ff2086a3972a377a7816926
David Minarsch <david.minarsch@googlemail.com>
2021-04-14 11:05:17 +0100

    Updates

---
+++ component---src-pages-404-js-0994e2411de52d4cfcdd.js
@@ -0,0 +1,2 @@
+(self.webpackChunkaea_web=self.webpackChunkaea_web||[]).push([[883],{9616:function(e,t,n){"use strict";n.r(t);var a=n(2331),u=n(2200),l=n(7294),r=n(3751);t.default=function(){return l.createElement(l.Fragment,null,l.createElement(r.Z,{title:"404: Not found"}),l.createElement(a.X,null,"404: Not Found"),l.createElement(u.x,{fontSize:"xl",my:5},"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0994e2411de52d4cfcdd.js.map
